17cb306cc3e40ee9a937dda1a9ebf37f505d202d
Leonardo <sleonardoaugusto@gmail.com>
2019-12-05 11:40:47 -0300

    First commit

---
+++ joyjet_cart_api/core/tests/test_core.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class ResponseTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """GET / must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
