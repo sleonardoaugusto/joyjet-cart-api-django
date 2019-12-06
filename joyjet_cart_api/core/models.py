3aae18929816ab4ba07dbf4cbc203a759b0dc1ac
Leonardo <sleonardoaugusto@gmail.com>
2019-12-05 23:58:10 -0300

    Ecommerce models setup

--- joyjet_cart_api/core/models.py
+++ joyjet_cart_api/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(null=True)
+
+
+class Cart(models.Model):
+    pass
+
+
+class Item(models.Model):
+    quantity = models.IntegerField(null=False)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
