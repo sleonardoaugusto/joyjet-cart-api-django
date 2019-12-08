471f69be8c676b7fe3c19c25e133e597983764a0
Leonardo <sleonardoaugusto@gmail.com>
2019-12-08 16:27:20 -0300

    View implemented

--- joyjet_cart_api/core/views.py
+++ joyjet_cart_api/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import (
+    Cart,
+)
+from joyjet_cart_api.core.serializers import (
+    CartSerializer,
+)
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
